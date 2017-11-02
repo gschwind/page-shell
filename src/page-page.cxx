e8ce6cfab45daf854e0b883a972f22c1f0f51d97
Benoit Gschwind <gschwind@gnu-log.net>
2017-11-02 19:58:46 +0100

    add page-data

--- src/page-page.cxx
+++ src/page-page.cxx
@@ -212,7 +212,7 @@ page_t::page_t(MetaPlugin * plugin) :
 	/* load configurations, from lower priority to high one */
 
 	/* load default configuration */
-	_conf.merge_from_file_if_exist(string{/*DATA_DIR*/ "/page/page.conf"});
+	_conf.merge_from_file_if_exist(string{GNOME_SHELL_DATADIR "/page.conf"});
 
 	/* load homedir configuration */
 	{
